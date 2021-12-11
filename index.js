6c3613206f3671b5372544f2a73b9b397170ca5b
Aaron Elias <aaronelias007@gmail.com>
2021-12-10 23:24:41 -0500

    commit tree 950274398a8b2fc171212c6b79fa4935ef9385d7

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup - hq)
+}
+function distanceFromHqInFeet (pickupinfeet) {
+  if (pickupinfeet > hq) {
+      return (pickupinfeet - hq) * feet; 
+  } else if (pickupinfeet < hq) {
+      return (hq - pickupinfeet) * feet;
+ }
+
+}
+function distanceTravelledInFeet(a, b){
+if (a > b){
+return (a - b) * feet 
+} else if (b > a){
+    return (b - a) * feet
+}
+
+}
+
+function calculatesFarePrice(start, destination){
+    if (start < destination){
+        if (((destination - start) * feet) < 400){
+             return (0)
+        } else if (((destination - start) * feet) < 2000){
+             return (((destination - start) * feet - 400) * 0.02) 
+        } else if (((destination - start) * feet) < 2500){
+             return (25)
+        } else if (((destination - start) * feet) >= 2500){
+             return ('cannot travel that far')
+}
+
+    } else if (start > destination){
+         if (((start - destination) * feet) < 400){
+             return (0)
+        } else if (((start - destination) * feet) < 2000)    {
+             return (((start - destination) * feet - 400) * 0.02) 
+        } else if (((start - destination) * feet) < 2500){
+             return (25)
+        } else if (((start - destination) * feet) >= 2500) {
+             return ('cannot travel that far')
+        }
+    }
+}
